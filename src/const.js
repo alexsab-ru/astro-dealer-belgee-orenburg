6c5990c19b3c0629f014f2b0e9e6494311b70a3d
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2025-08-04 13:01:08 +0500

    upd files

--- src/const.js
+++ src/const.js
@@ -30,8 +30,8 @@ export const LINKS_MENU = [
 	{url: 'special-offers/', name: 'Спецпредложения'},
 	// {url: 'news/', name: 'Новости'},
 	{url: 'test-drive/', name: 'Запись на тест-драйв'},
-	// {url: 'service-request/', name: 'Запись на сервис'},
-	{url: '#services', name: 'Услуги'},
+	{url: 'service-request/', name: 'Запись на сервис'},
+	// {url: '#services', name: 'Услуги'},
 	{url: 'contacts/', name: 'Контакты'},
 ];
 
